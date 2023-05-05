937400002392c6076b369812cd7e521006cbeaec
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-05-05 08:48:03 +0600

    New code added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
+// https://localhost:9999/v1.0.0/registration/admin/register
 
 function App() {
   const requestData = () => {
-    fetch(`https://localhost:9999/v1.0.0/registration/admin/register`, {
+    fetch(`https://35.223.101.95:8443/v1.0.0/registration/admin/register`, {
       method: "POST",
       headers: {
         "content-type": "application/json"
@@ -15,7 +16,7 @@ function App() {
         "address": "Demra1, Dhaka",
         "city": "Dhaka1",
         "contactnumber": "+918447925466",
-        "email": "aabb@gmail.com",
+        "email": "aalkui@gmail.com",
         "pincode": "242554",
         "notification": {
           "email": true,
